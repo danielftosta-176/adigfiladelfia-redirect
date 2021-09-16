e0c2a322598908bb9b09d0400acdb78d2a8e1901
danielftosta-176 <78708647+danielftosta-176@users.noreply.github.com>
2021-09-15 21:35:11 -0300

    Update update.js

--- casa/assets/js/update.js
+++ casa/assets/js/update.js
@@ -1,5 +1,6 @@
-var currentversion = "Em Manuntenção - Estilo novo do site!"
+var currentversion = "Adig Filadélfia."
 
 function loadhd(); {
 document.getElementById("warning-aa").innerHTML = currentversion
 };
+document.body.onload = function() {loadhd()};
